743fb934a1ddeb7c038c3d2a20d9381b14204eda
Podapati <suneel.podapati@accenture.com>
2017-11-07 17:30:06 +0530

    optional and multi level inheritance

---
+++ TestCSharp/Classes.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCSharp
+{
+    namespace OptionalParameters
+    {
+        public class OptionalVsOverload
+        {
+            public void M1(int a, char b, string c = "Optional") // I am not that important if I have a matching overload
+            {
+                Console.WriteLine("M1 Optional");
+            }
+            public void M1(int a, char b)  // I have the priority since I am an absolute match
+            {
+                Console.WriteLine("M1 Overload");
+            }
+        } 
+
+    }
+
+    namespace MultiLevelInheritance
+    {
+        public class A
+        {
+            public virtual void M1()
+            {
+                Console.WriteLine("I am A in : " + this + " : virtual");
+            }
+            public void M2()
+            {
+                Console.WriteLine("I am A in : " + this + " : concrete");
+            }
+
+            // AM1, AM2
+        }
+
+        public class B : A
+        {
+            public override void M1()
+            {
+                Console.WriteLine("I am B in : " + this + " : Override");
+            }
+            public new void M2()
+            {
+                Console.WriteLine("I am B in : " + this + " : new");
+            }
+
+            // BM1, AM2, BM2
+        }
+
+        public class C : B
+        {
+            public new void M1()
+            {
+                Console.WriteLine("I am C in : " + this + " : new");
+            }
+            public new virtual void M2()
+            {
+                Console.WriteLine("I am C in : " + this + " : new virtual");
+            }
+
+            // BM1, CM1, AM2, BM2, CM2
+        }
+
+        public class D : C
+        {
+            public new void M1()
+            {
+                Console.WriteLine("I am D in : " + this + " : Override");
+            }
+            public override void M2()
+            {
+                Console.WriteLine("I am D in : " + this + " : Override");
+            }
+
+            // BM1, CM1, DM1, AM2, BM2, DM2
+        }
+
+
+
+
+    }
+
+
+}
---
+++ TestCSharp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestCSharp.MultiLevelInheritance;
+using System.Threading.Tasks;
+using TestCSharp.OptionalParameters;
+
+namespace TestCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region OptionalParameters
+            //OptionalVsOverload ov = new OptionalVsOverload();
+            //ov.M1(1, 'a');
+            #endregion
+
+            #region MultiLevelInheritance
+            //A a = new A();
+            //a.M1();
+            //a.M2();
+            //B b = new B();
+            //b.M1();
+            //b.M2();
+            //C c = new C();
+            //c.M1();
+            //c.M2();
+            //D d = new D();
+            //d.M1();
+            //d.M2();
+
+            //a = (A)b;
+            //a.M1();
+            //a.M2();
+            //a = (A)c;
+            //a.M1();
+            //a.M2();
+            //a = (A)d;
+            //a.M1();
+            //a.M2(); 
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
